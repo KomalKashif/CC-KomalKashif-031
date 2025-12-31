389cd9613fe9e718bda7be7748bd997b21f9f4c2
KomalKashif <23-22411-031@se.fjwu.edu.pk>
2025-12-31 18:24:03 +0500

    Remove Assignment2 folder from repository but keep locally

--- Assignment2/modules/security/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "nginx_sg_id" {
-  description = "ID of Nginx security group"
-  value       = aws_security_group.nginx_sg.id
-}
-
-output "backend_sg_id" {
-  description = "ID of Backend security group"
-  value       = aws_security_group.backend_sg.id
-}
